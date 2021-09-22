090a64a88d83ff283f524ac7af16489af89c07e7
smeikx <noreply@smeikx.com>
2021-09-22 23:50:41 +0200

    start implementing efficient background rendering

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kitt Color Control
+
+This simple plugin is made for usage in Neovim’s TUI inside kitty, it does two things:
+
+- set the cursor’s foreground color according to the active color scheme
+- increase efficiency by setting the window’s background and foreground colors to the ones of the `Normal` highlight group, and setting the group’s colors to `NONE`
--- plugin/kitty-cursor.lua
+++ plugin/kitty-cursor.lua
@@ -1,18 +1,18 @@
 -- TODO: make this customisable
 local kitty_cmd = 'kitty @ --to=unix:@hellokitty'
 
-function set (hexColor)
+function setCursor (hexColor)
 	vim.fn.jobstart(string.format('%s set-colors cursor_text_color=\\#%s', kitty_cmd, hexColor))
 end
 
-function match ()
+function matchCursor ()
 	local guifg =
 		vim.api.nvim_exec('hi Cursor', true):match('guifg=#([a-f0-9]+)')
 		or vim.api.nvim_exec('hi Normal', true):match('guibg=#([a-f0-9]+)')
-	set(guifg)
+	setCursor(guifg)
 end
 
-function restore ()
+function restoreCursor ()
 	vim.fn.jobstart(([[
 		%s set-colors cursor_text_color='#'$( \
 			%s get-colors -c \
@@ -21,17 +21,55 @@ function restore ()
 	]]):format(kitty_cmd, kitty_cmd), {detach = true})
 end
 
-_G.kittyCursorColor = {
-	--set = set, -- uncomment if needed
-	match = match,
-	restore = restore
+local normal = {
+	bg = '',
+	fg = ''
+}
+function setNormal ()
+	vim.fn.jobstart(string.format(
+		'%s set-colors background=\\#%s foreground=\\#%s',
+		kitty_cmd, normal.bg, normal.fg)
+	)
+end
+
+function matchNormal ()
+	local scheme_colors = vim.api.nvim_exec('hi Normal', true)
+	normal.fg = scheme_colors:match('guifg=#([a-f0-9]+)')
+	normal.bg = scheme_colors:match('guibg=#([a-f0-9]+)')
+	vim.cmd('hi Normal NONE')
+	setNormal()
+end
+
+function restoreNormal ()
+end
+
+function restore ()
+	vim.fn.jobstart(([[
+		%s get-colors -c \
+		| grep -e 'cursor_text_color' -e '^background' -e '^foreground'
+		| sed -nE 's/.+#([a-f0-9]{6}).*/\1/p' \
+		| read bg cursor fg; %s set-colors cursor_text_color='#'$( \
+		)
+	]]):format(kitty_cmd, kitty_cmd), {detach = true})
+end
+
+_G.kcc = {
+	--set = setCursor, -- uncomment if needed
+	cursor = {
+		match = matchCursor,
+		restore = restoreCursor
+	},
+	normal = {
+		match = matchNormal,
+		restore = restoreNormal
+	}
 }
 
 vim.cmd([[
 	aug kitty_cursor
 		au!
-		au ColorScheme * call v:lua.kittyCursorColor.match()
-		au VimEnter,VimResume * call v:lua.kittyCursorColor.match()
-		au VimLeavePre,VimSuspend * call v:lua.kittyCursorColor.restore()
+		au ColorScheme * call v:lua.kcc.match()
+		au VimEnter,VimResume * call v:lua.kcc.match()
+		au VimLeavePre,VimSuspend * call v:lua.kcc.restore()
 	aug END
 ]])
